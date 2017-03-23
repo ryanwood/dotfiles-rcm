6d006f544352669fc824eb09e3000f9cc7a1a8ac
Ryan Wood <ryan.wood@gmail.com>
2017-03-23 13:53:43 -0400

    fix binstubs with chruby
    https://dance.computer.dance/posts/2015/02/making-chruby-and-binstubs-play-nice.html

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,11 +1,35 @@
-
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
-
 # ensure dotfiles bin directory is loaded first
-PATH="$HOME/.bin:/usr/local/sbin:$PATH"
+export PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
-# mkdir .git/safe in the root of repositories you trust
-PATH=".git/safe/../../bin:$PATH"
+# https://dance.computer.dance/posts/2015/02/making-chruby-and-binstubs-play-nice.html
 
-export -U PATH
+# Chruby
+if [[ -e /usr/local/opt/chruby/share/chruby/chruby.sh ]]; then
+  source /usr/local/opt/chruby/share/chruby/chruby.sh
+  source /usr/local/opt/chruby/share/chruby/auto.sh
+  [[ -r ~/.ruby-version ]] && chruby $(cat ~/.ruby-version)
+  [[ -r ./.ruby-version ]] && chruby $(cat ./.ruby-version)
+fi
+
+# Remove the need for bundle exec ... or ./bin/...
+# by adding ./bin to path if the current project is trusted
+
+function set_local_bin_path() {
+  # Replace any existing local bin paths with our new one
+  export PATH="${1:-""}`echo "$PATH"|sed -e 's,[^:]*\.git/[^:]*bin:,,g'`"
+}
+
+function add_trusted_local_bin_to_path() {
+  if [[ -d "$PWD/.git/safe" ]]; then
+    # We're in a trusted project directory so update our local bin path
+    set_local_bin_path "$PWD/.git/safe/../../bin:"
+  fi
+}
+
+# Make sure add_trusted_local_bin_to_path runs after chruby so we
+# prepend the default chruby gem paths
+if [[ -n "$ZSH_VERSION" ]]; then
+  if [[ ! "$preexec_functions" == *add_trusted_local_bin_to_path* ]]; then
+    preexec_functions+=("add_trusted_local_bin_to_path")
+  fi
+fi
