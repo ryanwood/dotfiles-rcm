f24fc98bf37441462a55ea37f1e08036daad46c4
Ryan Wood <ryan.wood@gmail.com>
2017-08-28 16:56:35 -0400

    organize zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,144 +1,64 @@
-alias reload="source ~/.zshrc"
-
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
-alias start="vim ~/start.txt"
-
-# Mac Helpers
-alias show_hidden="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
-alias hide_hidden="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
-alias pt='papertrail'
-
-# Rails
-alias devlog='tail -f log/development.log'
-alias prodlog='tail -f log/production.log'
-alias testlog='tail -f log/test.log'
-
-# Rails aliases
-alias rc='rails console'
-alias rcs='rails console --sandbox'
-alias rd='rails destroy'
-alias rdb='rails dbconsole'
-alias rg='rails generate'
-alias rgm='rails generate migration'
-alias rs='rails server'
-alias rsd='rails server --debugger'
-alias rsp='rails server --port'
-alias rst='touch tmp/restart.txt'
-
-# Rake aliases
-alias rdm='rake db:migrate'
-alias rdms='rake db:migrate:status'
-alias rdmtc='rake db:migrate db:test:clone'
-alias rdr='rake db:rollback'
-alias rdtc='rake db:test:clone'
-alias rdtp='rake db:test:prepare'
-alias rdsl='rake db:schema:load'
-alias rlc='rake log:clear'
-alias rmd='rake middleware'
-alias rn='rake notes'
-alias rr='rake routes'
-alias rrg='rake routes | grep'
-alias rsts='rake stats'
-alias rt='rake test'
-
-# RSpec aliases
-alias t="bundle exec rspec"
-alias tff="bundle exec rspec --fail-fast"
-alias tfo="bundle exec rspec --only-failures"
-
-# Bundler
 alias b="bundle"
-alias be="b exec"
-alias bl="b list"
-alias bp="b package"
-alias bo="b open"
-alias bout="b outdated"
-alias bu="b update"
 alias bcn="b clean"
+alias be="b exec"
 alias bi="b install --path vendor"
 alias bil="bi --local"
 alias binit="bi && b package && echo '\nvendor/ruby' >> .gitignore"
-
-# Deployment
-
-alias hrcp='heroku run console -r production'
-alias hrcs='heroku run console -r staging'
-alias ds='NO_CONFIRM=1 ./deploy staging'
-alias dp='./deploy production'
-
-# Git
-
-# No arguments: `git status`
-# With arguments: acts like `git`
-g() {
-  if [[ $# -gt 0 ]]; then
-    git "$@"
-  else
-    git status -sb
-  fi
-}
-# Pretty log messages
-function _git_log_prettily(){
-  if ! [ -z $1 ]; then
-    git log --pretty=$1
-  fi
-}
-# Warn if the current branch is a WIP
-function work_in_progress() {
-  if $(git log -n 1 2>/dev/null | grep -q -c "\-\-wip\-\-"); then
-    echo "WIP!!"
-  fi
-}
-
+alias bl="b list"
+alias bo="b open"
+alias bout="b outdated"
+alias bp="b package"
+alias bu="b update"
 
 alias d='git diff'
-alias s='git status -sb'
+alias devlog='tail -f log/development.log'
+alias ds='NO_CONFIRM=1 ./deploy staging'
+alias dp='./deploy production'
 
 alias ga='git add'
 alias gaa='git add --all'
 alias gau='git add --update'
 
 alias gb='git branch'
-alias gba='git branch -a'
-alias gbda='git branch --no-color --merged | command grep -vE "^(\*|\s*(master|develop|dev)\s*$)" | command xargs -n 1 git branch -d'
-alias gbl='git blame -b -w'
-alias gbnm='git branch --no-merged'
-alias gbr='git branch --remote'
-alias gbs='git bisect'
-alias gbsb='git bisect bad'
-alias gbsg='git bisect good'
-alias gbsr='git bisect reset'
-alias gbss='git bisect start'
+# alias gba='git branch -a'
+# alias gbda='git branch --no-color --merged | command grep -vE "^(\*|\s*(master|develop|dev)\s*$)" | command xargs -n 1 git branch -d'
+# alias gbl='git blame -b -w'
+# alias gbnm='git branch --no-merged'
+# alias gbr='git branch --remote'
+# alias gbs='git bisect'
+# alias gbsb='git bisect bad'
+# alias gbsg='git bisect good'
+# alias gbsr='git bisect reset'
+# alias gbss='git bisect start'
 alias gbx='git branch -d'
 alias gbX='git branch -D'
 
 alias gc='git commit -v'
 alias gc!='git commit -v --amend'
-alias gcn!='git commit -v --no-edit --amend'
 alias gca='git commit -v -a'
 alias gca!='git commit -v -a --amend'
+alias gcam='git commit -a -m'
+alias gcmsg='git commit -m'
 alias gcan!='git commit -v -a --no-edit --amend'
 alias gcans!='git commit -v -a -s --no-edit --amend'
-alias gcam='git commit -a -m'
-alias gcsm='git commit -s -m'
 alias gcb='git checkout -b'
 alias gcf='git config --list'
 alias gcl='git clone --recursive'
 alias gcln='git clean -fd'
-alias gpristine='git reset --hard && git clean -dfx'
 alias gcm='git checkout master'
-
-alias gcmsg='git commit -m'
+alias gcn!='git commit -v --no-edit --amend'
 alias gco='git checkout'
 alias gcount='git shortlog -sn'
 alias gcp='git cherry-pick'
 alias gcpa='git cherry-pick --abort'
 alias gcpc='git cherry-pick --continue'
 alias gcs='git commit -S'
+alias gcsm='git commit -s -m'
 
 alias gd='git diff'
 alias gdca='git diff --cached'
@@ -152,22 +72,24 @@ alias gfo='git fetch origin --prune'
 
 alias gl='glol'
 alias glg='git log --stat'
-alias glgp='git log --stat -p'
 alias glgg='git log --graph'
 alias glgga='git log --graph --decorate --all'
 alias glgm='git log --graph --max-count=10'
+alias glgp='git log --stat -p'
 alias glo='git log --oneline --decorate'
 alias glol="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias glola="git log --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
 alias glog='git log --oneline --decorate --graph'
 alias gloga='git log --oneline --decorate --graph --all'
 alias glp="_git_log_prettily"
+alias glum='git pull upstream master'
 
 alias gm='git merge --no-ff'
 alias gmf='git merge --ff-only'
 
 alias gp='git push'
 alias gpd='git push --dry-run'
+alias gpristine='git reset --hard && git clean -dfx'
 
 alias gr='git reset'
 alias gra='git remote add'
@@ -198,7 +120,73 @@ alias gsu='git submodule update'
 alias gunignore='git update-index --no-assume-unchanged'
 alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
 alias gup='git smart-pull'
-alias glum='git pull upstream master'
 
 alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify -m "--wip-- [skip ci]"'
+
+alias hide_hidden="defaults write com.apple.Finder AppleShowAllFiles -bool false && killall Finder"
+alias hrcp='heroku run console -r production'
+alias hrcs='heroku run console -r staging'
+
+alias prodlog='tail -f log/production.log'
+alias pt='papertrail'
+
+alias rc='rails console'
+alias rcs='rails console --sandbox'
+alias rd='rails destroy'
+alias rdb='rails dbconsole'
+alias rdm='rake db:migrate'
+alias rdms='rake db:migrate:status'
+alias rdmtc='rake db:migrate db:test:clone'
+alias rdr='rake db:rollback'
+alias rdtc='rake db:test:clone'
+alias rdsl='rake db:schema:load'
+alias rdtp='rake db:test:prepare'
+alias reload="source ~/.zshrc"
+alias rg='rails generate'
+alias rgm='rails generate migration'
+alias rs='rails server'
+alias rsd='rails server --debugger'
+alias rsp='rails server --port'
+alias rst='touch tmp/restart.txt'
+alias rlc='rake log:clear'
+alias rmd='rake middleware'
+alias rn='rake notes'
+alias rr='rake routes'
+alias rrg='rake routes | grep'
+alias rsts='rake stats'
+alias rt='rake test'
+
+alias s='git status -sb'
+alias show_hidden="defaults write com.apple.Finder AppleShowAllFiles -bool true && killall Finder"
+alias start="vim ~/start.txt"
+
+alias t="bundle exec rspec"
+alias td="tmux a -t deerlab"
+alias testlog='tail -f log/test.log'
+alias tff="bundle exec rspec --fail-fast"
+alias tfo="bundle exec rspec --only-failures"
+alias tm="tmux a -t moonclerk"
+# Git
+
+# No arguments: `git status`
+# With arguments: acts like `git`
+g() {
+  if [[ $# -gt 0 ]]; then
+    git "$@"
+  else
+    git status -sb
+  fi
+}
+# Pretty log messages
+function _git_log_prettily(){
+  if ! [ -z $1 ]; then
+    git log --pretty=$1
+  fi
+}
+# Warn if the current branch is a WIP
+function work_in_progress() {
+  if $(git log -n 1 2>/dev/null | grep -q -c "\-\-wip\-\-"); then
+    echo "WIP!!"
+  fi
+}
